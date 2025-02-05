473903af9966909b060daa3f322bcd77c75f0d45
Eden <edenhadar@gmail.com>
2025-02-05 15:32:12 -0800

    bar chart ex

---
+++ d3/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Barchart from './components/Barchart';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Hello React + D3 world!</h1>
+    <Barchart/>
+    </div>
+  );
+}
+
+export default App;
